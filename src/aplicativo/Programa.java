05d75db04e8a30e0f282f6bfa8f321cdbead4fcf
souzaoe <souzaoe@hotmail.com>
2020-12-27 14:16:58 -0300

    Acrescentado a mensagem bom dia

--- src/aplicativo/Programa.java
+++ src/aplicativo/Programa.java
@@ -3,7 +3,8 @@ package aplicativo;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("Bom dia, finalmente essa merda esta dando certo, caralho chato github");
 
 	}
 
